74506034b21cdd3047ce3d7451ed3c1666e202d3
theus520 <matheusrabelo520@gmail.com>
2020-06-07 21:34:47 -0300

    dando vida ao templente

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,30 @@
-function acao(obj){
+var obj = document.getElementById('ma');
+var foto =   document.getElementById("mab");
+function acao(){
     if(document.getElementById(obj).style.display == 'block'){
  
+    document.getElementById(foto).style.display = 'block';
     document.getElementById(obj).style.display = 'none';
  
     }else{
         document.getElementById(obj).style.display = 'block'; 
+        document.getElementById(foto).style.display = 'none';
+ 
+    }
+}
+function acao(){
+    if(document.getElementById(obj).style.display == 'block'){
+    document.getElementById(obj).style.display = 'none';
+ 
+    }else{
+        document.getElementById(obj).style.display = 'block'; 
+    
+
+
+        
+ 
+
+
+
     }
 }

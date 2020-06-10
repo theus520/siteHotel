1cc341b4602cb5920f6e4c91ce90c4e522e61d52
theus520 <matheusrabelo520@gmail.com>
2020-06-09 23:02:24 -0300

    dando andamento ao projeto

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,47 @@
-var obj = document.getElementById('ma');
-var foto =   document.getElementById("mab");
-function acao(){
-    if(document.getElementById(obj).style.display == 'block'){
- 
-    document.getElementById(foto).style.display = 'block';
-    document.getElementById(obj).style.display = 'none';
- 
-    }else{
-        document.getElementById(obj).style.display = 'block'; 
-        document.getElementById(foto).style.display = 'none';
- 
+
+function acao(cadast,foto,inf){
+    if(document.getElementById(cadast).style.display == 'block'){
+
+        document.getElementById(inf).style.display = 'block'; 
+        document.getElementById(foto).style.display = 'block'; 
+        document.getElementById(cadast).style.display = 'none';
+    }else{ 
+        document.getElementById(cadast).style.display = 'block'; 
+        document.getElementById(foto).style.display = 'none'; 
+        document.getElementById(inf).style.display = 'none'; 
+
     }
-}
-function acao(){
-    if(document.getElementById(obj).style.display == 'block'){
-    document.getElementById(obj).style.display = 'none';
- 
-    }else{
-        document.getElementById(obj).style.display = 'block'; 
-    
+    }
+    function acaoFotos(cadast,foto,inf){
+        if(document.getElementById(cadast).style.display == 'block'){
 
-
+         
+            document.getElementById(foto).style.display = 'none';
+            document.getElementById(inf).style.display = 'none'; 
+        }else{ 
         
- 
+            document.getElementById(inf).style.display = 'block'; 
+            document.getElementById(foto).style.display = 'block'; 
+         
+        }
+        }
+        function acaoInformacao(cadast,inf, foto){
 
+            if(document.getElementById(cadast).style.display == 'block'){
 
+                document.getElementById(foto).style.display = 'none';  
+                document.getElementById(inf).style.display = 'none';
+              
+            }else{ 
+            
+                document.getElementById(foto).style.display = 'block'; 
+                document.getElementById(inf).style.display = 'block'; 
+               
+            }
+            }
+        
+        
 
-    }
-}
+       
+          
+            
---
+++ paginaPrincipal.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        	
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+        <link rel="stylesheet" type="text/css" href="css/estilomenu.css" />
+        <script type="text/javascript" src="javascript/script.js"></script>
+        <script type="text/javascript" src="javascript/validaCampo.js"></script>
+        <script type="text/javascript" src="javascript/viaCep.js"></script>
+    </head>
+    <body>
+        <form name="validaheader" class="header" method="$_POST" action="caluloDiaria.php">
+            <div id="check">
+            Pessoas
+            <input class="in_date" type="text" id="myInput" placeholder="0 pessoas" oninput="myFunction()">
+            check-in
+            <input class="in_date" type="date" id="Input" placeholder="0 pessoas" oninput="myFunction()">
+          
+            check-on
+            <input class="in_date" type="date">
+            <p id="dem"></p>
+            <p id="demo"></p>
+            <script>
+            function myFunction() {
+                var x = document.getElementById("myInput").value;
+                     document.getElementById("dem").innerHTML ="NA"+ x ;
+}
+function myFunction() {
+                var a = document.getElementById("Input").value;
+                 document.getElementById("demo").innerHTML =   a ;
+}
+</script>
+</div>
+        </form>
+
+        
+        
+        
+        <div class="sidenav">
+            <input type="checkbox" id="chec"/>
+         <label for="chec">
+             <img src="imagens/download.png"/>
+         </label>
+        </form>
+         <nav >
+            <div id="ul">
+    
+                <input type="button" class="menuButton" onclick="acao('cadast','foto','inf')" value="Cadastro Usuário"/>
+                <div>
+      <form name="form" id="cadast" class="hidden" onSubmit="return validacao();" action="dados.php" method="$_GET">
+        
+                 <input  name="nome"  type="text" placeholder="               Nome" />
+        
+                 <input name="email" type="email" name="email"placeholder="               E-mail" />
+            
+                 <input name="cpf"  type="text" name="cpf" placeholder="               CPF"/>
+            
+                 <input name="telefone"  type="text" name="telefone" placeholder="               Telefone" />
+            
+                 <input name="cep"  type="text" name="telefone" placeholder="               CEP" id= "cep" onblur="pesquisacep(this.value);"/>
+                
+                 <input name="cidade"  type="text" name="bairro" placeholder="               Cidade"id="cidade" />
+                
+                 <input name="bairro"  type="text" name="bairro" placeholder="               Bairro"id="bairro" />
+            
+                 <input name="lograduoro"  type="text" name="logradouro"  placeholder="               Logradouro" id="rua"/>
+            
+                 <input name="numero"  type="number" name="numero" placeholder="               Número" />
+
+                 <input  class="menuButton"type="submit" name="enviar" />
+                </form>
+       <div id="foto"  class="hidde">
+        <input type="button" class="menuButton" onclick=" acaoFotos('inf','foto','cadast')" value="Fotos"/>
+       </div>
+            <div id="inf" >
+                <input type="button" class="menuButton" onclick="acaoInformacao('inf','foto','cadast')" value="Informacoes"/>
+                <div>
+         </div>
+      </div> 
+</nav>
+
+    <div id="fixed">
+    <iframe  
+    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCSO45Jd8GCAAN9KoKQAbWL4G-GWifMr1Y &q=Pousada Dunas do Jalapão" allowfullscreen>
+ 
+ </iframe> 
+</div>
+        </div>
+        <div class="content">
+            <h2 style="text-align: center;margin-top: 200px;">CSS Template</h2>
+            
+            <p id="pagina"></p>
+            <script>
+                document.getElementById("pagina").innerHTML = outraFuncao();
+            </script>
+          </div>
+          
+        
+     </body>
+</html>
